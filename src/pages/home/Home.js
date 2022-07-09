7e0996b4ff524994003f3d18f3c87ed1e8d36c33
priscillacho <priscilla_cho@hotmail.com>
2022-07-08 18:59:31 -0700

    testing part 1

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,12 @@ import "./Home.css";
 function Home() {
     return (
         <div>
-            <h1>welcome, I'm Priscilla. nice to meet you :)</h1>
+            <h1>welcome! I'm Priscilla, nice to meet you :)</h1>
+            <ul>
+                <li>one</li>
+                <li>2</li>
+                <li>three</li>
+            </ul>
         </div>
     );
 }
