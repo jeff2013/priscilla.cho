e3f08e4edb85e9f119c3a6422b6ad61d7dc5d060
priscillacho <priscilla_cho@hotmail.com>
2022-07-15 14:33:23 -0700

    updating text on home pg

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,11 +4,7 @@ function Home() {
     return (
         <div>
             <h1>welcome! I'm Priscilla, nice to meet you :)</h1>
-            <ul>
-                <li>one</li>
-                <li>2</li>
-                <li>three</li>
-            </ul>
+            <p>currently under construction, good stuff coming soon</p>
         </div>
     );
 }
